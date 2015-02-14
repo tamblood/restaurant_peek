711846478ba8e180064e8aab7c1990fee2586c7a
Tam Blood <tamblood@gmail.com>
2015-02-14 14:04:11 -0800

    added fix for cheeseboard website parsing change

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -27,19 +27,19 @@ class Restaurant < ActiveRecord::Base
     # parse html
     doc = Nokogiri::HTML(open('http://www.cheeseboardcollective.coop/pizza'))
 
-    date_tues = doc.xpath('//*[@id="body"]/div/div[2]/div[1]/h4[2]').text
+    date_tues = doc.xpath('//*[@id="body"]/div/div[2]/div[1]/h4[1]').text
     pod_tues = doc.xpath('//*[@id="body"]/div/div[2]/div[1]/p[1]').text
 
-    date_wed = doc.xpath('//*[@id="body"]/div/div[2]/div[1]/h4[3]').text
+    date_wed = doc.xpath('//*[@id="body"]/div/div[2]/div[1]/h4[2]').text
     pod_wed = doc.xpath('//*[@id="body"]/div/div[2]/div[1]/p[2]').text
 
-    date_thurs = doc.xpath('//*[@id="body"]/div/div[2]/div[1]/h4[4]').text
+    date_thurs = doc.xpath('//*[@id="body"]/div/div[2]/div[1]/h4[3]').text
     pod_thurs = doc.xpath('//*[@id="body"]/div/div[2]/div[1]/p[3]').text
 
-    date_fri = doc.xpath('//*[@id="body"]/div/div[2]/div[1]/h4[5]').text
+    date_fri = doc.xpath('//*[@id="body"]/div/div[2]/div[1]/h4[4]').text
     pod_fri = doc.xpath('//*[@id="body"]/div/div[2]/div[1]/p[4]').text
 
-    date_sat = doc.xpath('//*[@id="body"]/div/div[2]/div[1]/h4[6]').text
+    date_sat = doc.xpath('//*[@id="body"]/div/div[2]/div[1]/h4[5]').text
     pod_sat = doc.xpath('//*[@id="body"]/div/div[2]/div[1]/p[5]').text
 
     # no pizza of the day on monday or sunday
@@ -50,7 +50,7 @@ class Restaurant < ActiveRecord::Base
     if today.wday == 1 || today.wday == 7
       special_of_day = "There is no pizza of the day on Monday or Sunday"
     else
-      special_of_day = week_pizza[today.wday][0]
+      special_of_day = week_pizza[today.wday - 1][0]
     end
 
     # return special
